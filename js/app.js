177abfc39fe67ff66b8cf2794036a2d82f61fa81
Talha Razzaq <tctnpk@gmail.com>
2023-01-26 21:54:49 +0500

    almost complete

---
+++ js/app.js
@@ -0,0 +1,66 @@
+showNotes();
+let addBtn = document.getElementById("addBtn");
+let addNoteText = document.getElementById("addNoteText");
+
+// add note button listener
+addBtn.addEventListener("click", function () {
+  textValue = addNoteText.value;
+
+  let storedNotes = localStorage.getItem("notes");
+
+  let notesArr = [];
+  if (storedNotes != null) {
+    notesArr = JSON.parse(storedNotes);
+  } else {
+    notesArr = [];
+  }
+
+  if (textValue != "") {
+    notesArr.unshift(textValue);
+  }
+  addNoteText.value = "";
+  localStorage.setItem("notes", JSON.stringify(notesArr));
+  showNotes();
+});
+
+// function to show notes from the local storage
+function showNotes() {
+  let elem = document.getElementById("notes");
+  elem.innerHTML = "";
+  let storedNotes = localStorage.getItem("notes");
+  let notesArr = [];
+  if (storedNotes != null) {
+    notesArr = JSON.parse(storedNotes);
+  } else {
+    notesArr = [];
+  }
+  if (notesArr.length > 0) {
+    notesArr.forEach((note, index) => {
+      elem.innerHTML += `
+        <div id="note-${index}" class="card  m-2" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">${index + 1}</h5>
+                <p class="card-text">${note}</p>
+                <button onclick="delNote(${index})" class="btn btn-primary">Delete</button>
+            </div>
+        </div>
+         `;
+    });
+  } else{
+    elem.innerHTML= '<em style="color: grey">Nothing to show, Add some notes to show here</em>';
+  }
+}
+
+// delete button event listener
+function delNote(n) {
+  let storedNotes = localStorage.getItem("notes");
+  let notesArr = [];
+  if (storedNotes != null) {
+    notesArr = JSON.parse(storedNotes);
+  } else {
+    notesArr = [];
+  }
+  notesArr.splice(n, 1);
+  localStorage.setItem("notes", JSON.stringify(notesArr));
+  showNotes();
+}
